a88602e180e5a89a0b688c43ec1ffb44324f52a7
boot1110001 <boot111.0001@gmail.com>
2019-08-27 23:05:03 +0200

    it works

--- public_html/js/utils.js
+++ public_html/js/utils.js
@@ -1,5 +1,10 @@
 
-var switch_visible_status = true;
+var
+    switch_visible_status = true,
+    iterator = 0,
+    keys = [],
+    phrases = [],
+    result = {};
 
 function switch_visible() {
     var
@@ -9,7 +14,6 @@ function switch_visible() {
     if (switch_visible_status == true) {
         var i = 0;
         while (i < objs_visible.length) {
-            console.log('hola1');
             objs_visible[i].style.display = "none";
             i++;
         }
@@ -23,7 +27,6 @@ function switch_visible() {
     } else {
         var i = 0;
         while (i < objs_visible.length) {
-            console.log('hola2');
             objs_visible[i].style.display = "block";
             i++;
         }
@@ -37,10 +40,50 @@ function switch_visible() {
     }
 }
 
+function set_phrase() {
+    var
+        actual_key_cont = document.getElementById('actual_key'),
+        actual_text_cont = document.getElementById('actual_text'),
+        output_cont = document.getElementById('output');
+    actual_key_cont.innerText = keys[iterator];
+    actual_text_cont.innerText = phrases[keys[iterator]];
+    output_cont.value = JSON.stringify(result, null, 4);
+}
+
 function begin_btn() {
-    switch_visible();
+    var json = document.getElementById('input').value;
+
+    if (JSON.parse(json)) {
+        phrases = JSON.parse(json)['english'];
+        keys = Object.keys(phrases);
+        set_phrase();
+        switch_visible();
+    } else {
+        console.log('[FAIL] Thre is no json text in the input.');
+    }
 }
 
 function return_btn() {
     switch_visible();
+}
+
+function next_btn() {
+    var new_text_cont = document.getElementById('new_text');
+    result[keys[iterator]] = new_text_cont.value;
+
+    //cambiar next por end antes del último paso, y luego que desaparezca.
+
+    iterator++;
+
+    if (iterator < keys.length) {
+        set_phrase();
+    } else if (iterator == keys.length) {
+        var output_cont = document.getElementById('output');
+        console.log('[INFO] Fin');
+        output_cont.value = JSON.stringify(result, null, 4);
+    }
+}
+
+function copy_btn() {
+    console.log('copy...');
 }
