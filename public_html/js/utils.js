af430d147c330f098e7b6a2dad9c4666cb25e6bd
boot1110001 <boot111.0001@gmail.com>
2019-08-27 17:08:11 +0200

    listo por el momento

--- public_html/js/utils.js
+++ public_html/js/utils.js
@@ -1,24 +1,46 @@
 
+var switch_visible_status = true;
 
 function switch_visible() {
     var
         objs_visible = document.getElementsByClassName("start_visible"),
         objs_hidden = document.getElementsByClassName("start_hidden");
 
-    var i = 0;
-    while (i < objs_visible.length) {
-        console.log('hola');
-        objs_visible[i].style.display = "none";
-        i++;
-    }
+    if (switch_visible_status == true) {
+        var i = 0;
+        while (i < objs_visible.length) {
+            console.log('hola1');
+            objs_visible[i].style.display = "none";
+            i++;
+        }
 
-    i = 0;
-    while (i < objs_hidden.length) {
-        objs_hidden[i].style.display = "block";
-        i++;
+        i = 0;
+        while (i < objs_hidden.length) {
+            objs_hidden[i].style.display = "block";
+            i++;
+        }
+        switch_visible_status = false;
+    } else {
+        var i = 0;
+        while (i < objs_visible.length) {
+            console.log('hola2');
+            objs_visible[i].style.display = "block";
+            i++;
+        }
+
+        i = 0;
+        while (i < objs_hidden.length) {
+            objs_hidden[i].style.display = "none";
+            i++;
+        }
+        switch_visible_status = true;
     }
 }
 
-function begin() {
+function begin_btn() {
+    switch_visible();
+}
+
+function return_btn() {
     switch_visible();
 }
